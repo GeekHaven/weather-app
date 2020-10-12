7b90159af5697fff1d99456da0b6814614253dbc
alkatrivedi <alkatrivedi12dec@gmail.com>
2020-10-12 11:51:42 +0530

    Updated home page with containing weather app details

---
+++ react/src/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+function Title() {
+  return (
+    <div className="Title">
+      <header>
+        <h1>ReactJs Weather App</h1>
+        <div className="Title-Subtitle">
+          A weather data app that fetches weather of a particular location with
+          the help of an API
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Title;
