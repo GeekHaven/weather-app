7fd85fab3d02b47cbf7315ed4c4ca5b23967c2b3
abhishek bhaware <abhishek.jee2019@gmail.com>
2020-10-12 18:21:04 +0530

    added footer

--- react/src/App.js
+++ react/src/App.js
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './App.css';
 import Title from "./Title";
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -47,6 +48,7 @@ function App() {
           <CurrentLocation />
         </Route>
       </Switch>
+      <Footer/>
     </Router>
     </div>
   );
