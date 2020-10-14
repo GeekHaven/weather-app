f1cad91251e463576eb453d8d4b472782dcc2bce
Tanish Agarwal <tanishagarwal81201@gmail.com>
2020-10-14 10:43:17 +0530

    registered model

--- django/weatherApp/subscribe/admin.py
+++ django/weatherApp/subscribe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+# superuser credentials :
+# Username: geekhaven
+# Password: geekhaven123
+admin.site.register(User)
