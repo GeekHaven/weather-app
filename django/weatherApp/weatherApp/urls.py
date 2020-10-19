c6819a6ebdb84f062b49e88dedbb24fdc95731d9
Anjali <nova.anjali@gmail.com>
2020-10-20 00:07:11 +0530

    Save data to django model

--- django/weatherApp/weatherApp/urls.py
+++ django/weatherApp/weatherApp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from weatherApp.views import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',
+         views.api, name='api'),
 ]
