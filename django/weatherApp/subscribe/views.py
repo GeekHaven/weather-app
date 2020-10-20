f7fba364a85b3cf11b2432001418339b595b0896
Anjali <nova.anjali@gmail.com>
2020-10-20 22:47:57 +0530

    Update views.py

--- django/weatherApp/subscribe/views.py
+++ django/weatherApp/subscribe/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
 
-import os
 import requests
-from requests.auth import HTTPBasicAuth
+from django.shortcuts import render
+from .models import User
+
 
 def api(request):
     name = request.POST.get('name')
     email = request.POST.get('email')
-
-    url = os.environ.get("URL", 'http://myhost:port/projectname/api/addposition?compName=Google&category=Developer')
-    url = "%s" % (url)
-    body = {"name" : "%s" % name, "email" : "%s" % email}
-        response = requests.post(url, auth=HTTPBasicAuth('USER', 'PASSWORD'), headers={'Content-Type': 'application/json'}, json=body)
-    if response.status_code == 200:
-        print("Code 200")
-    else:
-        print("Code not 200")
