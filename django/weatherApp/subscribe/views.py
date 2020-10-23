ffe8c85e4178c9a0c2e2c57c08d0bc906078638f
Anjali <nova.anjali@gmail.com>
2020-10-23 17:21:40 +0530

    Update views.py

--- django/weatherApp/subscribe/views.py
+++ django/weatherApp/subscribe/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+# render index.html of React app, which will render other js bundle files
+# 'frontend/build/index.html' will be rendered
+catchall = TemplateView.as_view(template_name='index.html')
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse(<h1>"Hello world!"</h1>)
